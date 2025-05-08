bae09cfc53b76b750fed6d568323bd8e362151fa
Nos <zvezdazanatih@gmail.com>
2025-05-08 10:04:24 +0700

    0

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const f5 = document.querySelector('.f5')
+const r5 = document.querySelector('.result5')
+
+  fetch('http://truruki.ru/api')
+    .then(response => response.json())
+    .then(j => {
+  
+      const a1=j[0];
+      const html = a1.map(item => `<tr><td>${item.name}</td><td>${item.age}</td><tr>`).join('');
+      r5.innerHTML =`<table> ${html} </table>`;
+      
+})
+
+
+/*
+const form = document.getElementById('form1');
+  form.addEventListener('submit', function(event) {
+    // event.preventDefault();
+    form.reset(); // очищаем форму
+  });
+
+  */
