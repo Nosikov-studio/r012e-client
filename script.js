8aa96cadce76b1a7c5b7eb07f9214c7d1a98a7f0
Nos <zvezdazanatih@gmail.com>
2025-05-08 14:56:07 +0700

    13

--- script.js
+++ script.js
@@ -11,11 +11,29 @@ const r5 = document.querySelector('.result5')
 })
 
 
-/*
-const form = document.getElementById('form1');
-  form.addEventListener('submit', function(event) {
-    // event.preventDefault();
-    form.reset(); // очищаем форму
-  });
 
-  */
+const form1 = document.getElementById('form1');
+  form1.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+  const formData = new FormData(form1); // Сбор данных формы
+  const FormDataObject = Object.fromEntries(formData);
+
+  fetch('http://truruki.ru/api/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      't':'b'
+    },
+    body: JSON.stringify({
+      ...FormDataObject,
+      views: 0,
+    })
+  })
+    .then(response => response.json())
+    .then(j => {
+      console.log(j);
+      ab();
+      form1.reset(); // очищаем форму
+    });
+  });
